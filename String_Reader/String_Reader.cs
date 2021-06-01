045cc73e95be58f6435b07bbd53b75cc27a56738
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-01 15:54:48 +0530

    Add files via upload

---
+++ String_Reader/String_Reader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace String_Reader
+{
+    class String_Reader
+    {
+        static void Main(string[] args)
+        {
+            StringWriter str = new StringWriter();
+            str.WriteLine("Hello, this message is read by StringReader class");
+            str.Close();
+            StringReader reader = new StringReader(str.ToString());       // Creating StringReader instance and passing StringWriter  
+            while (reader.Peek() > -1)                                    // Reading data 
+            {
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+    }
+}
