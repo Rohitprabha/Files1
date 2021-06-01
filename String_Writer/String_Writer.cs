84b87c5a1f8aff3af584f8eae1e35238af5d9f12
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-01 15:56:17 +0530

    Add files via upload

---
+++ String_Writer/String_Writer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.IO;
+namespace String_Writer
+{
+    class String_Writer
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello. This is Line 1 \n Hello.This is Line 2. \n Hello This is Line 3";
+            StringBuilder sb = new StringBuilder();                             // Creating StringBuilder instance   
+            StringWriter writer = new StringWriter(sb);                         // Passing StringBuilder instance into StringWriter 
+            writer.WriteLine(text);                                             // Writing data using StringWriter 
+            writer.Flush();  
+            writer.Close();                                                     // Closing writer connection  
+            StringReader reader = new StringReader(sb.ToString());              // Creating StringReader instance and passing StringBuilder
+            while (reader.Peek() > -1)
+            {
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+    }
+}
